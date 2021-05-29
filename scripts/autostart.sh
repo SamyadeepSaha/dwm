998b6e0f2d2604e24fa42d51749fd9352d45b3a2
Samyadeep Saha <samyadeep98@gmail.com>
2021-05-29 09:30:16 +0530

    Change fonts

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #pulseaudio --start
-xwallpaper --zoom ~/Downloads/338191.jpg
+xwallpaper --zoom /home/ak98/Downloads/wallpaperflare.com_wallpaper.jpg
 
 
 # status bar
@@ -10,14 +10,14 @@ while true; do
     bat_cap=$(cat /sys/class/power_supply/BAT1/capacity)
     bat_stat=$(cat /sys/class/power_supply/BAT1/status)
 	bat="[B:$bat_cap%$bat_stat]"
-	net="[C:$(nmcli dev status | grep connected | awk '{c=","}{print$2c$4}')]"
-	vol="[V:$(pamixer --get-volume-human)]"
-	stat="$vol$net$bat$dt"
-	xsetroot -name "$stat"
     if [[ $bat_cap -lt 10 ]] && [ $bat_stat = "Discharging" ]
     then
         notify-send -u critical "Battery Low"
     fi
+	net="[C:$(nmcli dev status | grep connected | awk '{c=","}{print$2c$4}')]"
+	vol="[V:$(pamixer --get-volume-human)]"
+	stat="$vol$net$bat$dt"
+	xsetroot -name "$stat"
 	sleep 1s
 done
 
